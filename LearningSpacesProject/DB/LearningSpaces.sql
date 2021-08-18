a49f6245d09dc4fe5a8f33f9d2b0b228376e2abd
Popovski <Popovski@DESKTOP-B9IK67H.osz.intern>
2021-08-18 10:36:11 +0200

    LearningSpaces: Added Room Table Hristo

--- LearningSpacesProject/DB/LearningSpaces.sql
+++ LearningSpacesProject/DB/LearningSpaces.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `learningspaces` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `learningspaces`;
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: learningspaces
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,8 +33,8 @@ CREATE TABLE `a-g` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `user_idx` (`userID`),
   KEY `group_idx` (`groupID`),
-  CONSTRAINT `group` FOREIGN KEY (`groupID`) REFERENCES `groups` (`id`),
-  CONSTRAINT `user` FOREIGN KEY (`userID`) REFERENCES `accounts` (`id`)
+  CONSTRAINT `group` FOREIGN KEY (`groupID`) REFERENCES `group` (`id`),
+  CONSTRAINT `user` FOREIGN KEY (`userID`) REFERENCES `account` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,13 +48,13 @@ LOCK TABLES `a-g` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `account`
 --
 
-DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `accounts` (
+CREATE TABLE `account` (
   `id` int unsigned NOT NULL,
   `type` varchar(45) NOT NULL,
   `username` varchar(45) NOT NULL,
@@ -70,22 +70,22 @@ CREATE TABLE `accounts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `accounts`
+-- Dumping data for table `account`
 --
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `groups`
+-- Table structure for table `group`
 --
 
-DROP TABLE IF EXISTS `groups`;
+DROP TABLE IF EXISTS `group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `groups` (
+CREATE TABLE `group` (
   `id` int unsigned NOT NULL,
   `type` varchar(45) NOT NULL,
   `owner` int unsigned NOT NULL,
@@ -97,17 +97,41 @@ CREATE TABLE `groups` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
   KEY `owner_idx` (`owner`),
-  CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `accounts` (`id`)
+  CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `account` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `groups`
+-- Dumping data for table `group`
 --
 
-LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `ID` int NOT NULL,
+  `RoomNr` varchar(45) DEFAULT NULL,
+  `AdditionalInfo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -119,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-18 10:09:43
+-- Dump completed on 2021-08-18 10:34:14
