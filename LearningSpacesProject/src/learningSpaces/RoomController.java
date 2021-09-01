d34182b6b9739ac8a5167449e27dca1d995cf36a
JF <JF@JF-PC.osz.intern>
2021-09-01 09:45:31 +0200

    Jonathan: UI-Room

--- LearningSpacesProject/src/learningSpaces/RoomController.java
+++ LearningSpacesProject/src/learningSpaces/RoomController.java
@@ -1,5 +1,32 @@
 package learningSpaces;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class RoomController {
+	
+	@FXML
+    private TableView<?> tvRooms;
+
+    @FXML
+    private TableColumn<?, ?> tcRoomID;
+
+    @FXML
+    private TableColumn<?, ?> tcRoomName;
+
+    @FXML
+    private TableColumn<?, ?> tcAdditionalInfo;
+
+    @FXML
+    private TextArea taAdditionalInfo;
+
+    @FXML
+    private TextField tfRoomNumber;
+
+    @FXML
+    private TextField tfRommID;
 	
 }
