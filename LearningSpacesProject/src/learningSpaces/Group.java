e461f382d19bb916d85d8ea1de3a4bd930b87f29
jimmy <jimmy@DESKTOP-7BIRCBM.osz.intern>
2021-09-01 12:17:10 +0200

    Group Class

--- LearningSpacesProject/src/learningSpaces/Group.java
+++ LearningSpacesProject/src/learningSpaces/Group.java
@@ -3,6 +3,10 @@
  */
 package learningSpaces;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 /**
  * @author jimmy
  *
@@ -16,6 +20,83 @@ public class Group
 	private String subejct;
 	private String additionalInfo;
 	private String additionalData;
+	
+// Create(Insert)/Update/Delete	
+
+		public void create(int id, String type, int owner, String name,	String subejct, String additionalInfo, String additionalData)	
+		{		
+			Connection conn = DBConnection.getConnection();		
+			Statement lStatement;		
+			String lStatmentString;
+			
+			try 		
+			{	         
+				PreparedStatement ps = conn.prepareStatement("insert into booking values(?,?,?,?,?,?,?)");          
+				ps.setString(2, "");	         
+				ps.setString(4, "");	         
+				ps.setString(5, "");	                   
+				ps.setString(6, "");	        
+				ps.setString(7, "");	          
+				ps.setInt(1, 0);	         
+				ps.setInt(3, 0);	          
+				ps.executeUpdate();		
+	            System.out.println("insert successfully");								
+			} 
+			catch (Exception ex)          
+			{
+            	System.out.println("Error at the execution + " + "n" + ex.getMessage());         
+			}
+		}
+		public void update(int id, String type, int owner, String name,	String subejct, String additionalInfo, String additionalData)	
+		{		
+			Connection conn = DBConnection.getConnection();		
+			Statement lStatement;		
+			String lStatmentString;
+			
+			try 
+			{
+		           String updateQuery = "update booking set id=?,type=?,owner=?,  name=?, subject=?, additionalInfo=?, additionalData=? where id='";
+		            PreparedStatement ps1 = conn.prepareStatement(updateQuery);
+		            ps1.setString(2, "");
+		            ps1.setString(4, "");
+		            ps1.setString(5, "");
+		            ps1.setString(6, "");
+		            ps1.setString(7, "");
+		            ps1.setInt(1, 0);
+		            ps1.setInt(1, 0);
+		            ps1.executeUpdate();    
+		            System.out.println("updated successfully");	
+			}         
+			catch (Exception ex)          
+			{
+            	System.out.println("Error at the execution + " + "n" + ex.getMessage());         
+			}
+		}
+		public void delete(int id, String type, int owner, String name,	String subejct, String additionalInfo, String additionalData)	
+		{		
+			Connection conn = DBConnection.getConnection();		
+			Statement lStatement;		
+			String lStatmentString;
+			
+			try 
+			{
+		           String updateQuery = "delete booking set id=?,type=?,owner=?,  name=?, subject=?, additionalInfo=?, additionalData=? where id='";
+		            PreparedStatement ps1 = conn.prepareStatement(updateQuery);
+		            ps1.setString(2, "");
+		            ps1.setString(4, "");
+		            ps1.setString(5, "");
+		            ps1.setString(6, "");
+		            ps1.setString(7, "");
+		            ps1.setInt(1, 0);
+		            ps1.setInt(1, 0);
+		            ps1.executeUpdate();    
+		            System.out.println("delete successfully");	
+			}         
+			catch (Exception ex)          
+			{
+            	System.out.println("Error at the execution + " + "n" + ex.getMessage());         
+			}
+		}
 	//get/set
 	public int getId() {
 		return id;
@@ -62,3 +143,4 @@ public class Group
 	}
 	
 }
+
