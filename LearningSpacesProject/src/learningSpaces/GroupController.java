1cf028423020d9e7d6629a76c6b32a656a413df4
Popovski <Popovski@DESKTOP-B9IK67H.osz.intern>
2021-09-01 09:06:10 +0200

    Create Group UI: Hristo

--- LearningSpacesProject/src/learningSpaces/GroupController.java
+++ LearningSpacesProject/src/learningSpaces/GroupController.java
@@ -2,7 +2,7 @@ package learningSpaces;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField; 
+import javafx.scene.control.TextField;  
 
 public class GroupController {
 
