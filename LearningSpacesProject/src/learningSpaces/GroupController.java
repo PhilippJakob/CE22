886df4624bee43ab7bde603c4353d6687695a17a
Popovski <Popovski@DESKTOP-B9IK67H.osz.intern>
2021-09-01 13:13:04 +0200

    Group UI: Hristo

--- LearningSpacesProject/src/learningSpaces/GroupController.java
+++ LearningSpacesProject/src/learningSpaces/GroupController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.event.*; 
 
 public class GroupController {
 
