4927c5dcdc7e150d9f360617b62e276e47a65afe
JF <JF@JF-PC.osz.intern>
2021-09-01 12:25:32 +0200

    Jonathan: remove module-info

--- LearningSpacesProject/src/learningSpaces/Main.java
+++ LearningSpacesProject/src/learningSpaces/Main.java
@@ -11,13 +11,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("RoomView.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("RoomView.fxml")); 
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
-			e.printStackTrace();
+			e.printStackTrace(); 
 		}
 	}
 	
