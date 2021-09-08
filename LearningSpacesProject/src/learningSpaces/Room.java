9e3dc9f6fe15cfc7a8d54a9462f6501076eaaee0
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Admin <bunte.l@icloud.com>
2021-09-08 10:08:36 +0200

    added update(), insert(), delete()

--- LearningSpacesProject/src/learningSpaces/Room.java
+++ LearningSpacesProject/src/learningSpaces/Room.java
@@ -8,6 +8,7 @@ public class Room
    private int	  id;
    private String name;
    private String additionalInfo;
+   private static Connection conn;// = DBConnection.getConnection();
 
    public Room(int pId, String pName)
    {
@@ -28,14 +29,15 @@ public class Room
    
  //method to connect to database returns all entries in the room table learningspaces db as a ArrayList
    public static ArrayList<Room> readDB(Connection pConnection)
-   {      
+   {
+	 conn = DBConnection.getConnection();
 	 Room lSingleRoomData;
      ArrayList<Room> lRoomData = new ArrayList<Room>();
      Statement lStatement;
      ResultSet lResultSet;
 
      try {
-     lStatement = pConnection.createStatement();
+     lStatement = conn.createStatement();
      lResultSet = lStatement.executeQuery("SELECT * FROM room");
      lResultSet.first();  //Points to the first entry in the result set.
 
@@ -43,8 +45,9 @@ public class Room
         {
     	lSingleRoomData = new Room(lResultSet.getInt(1), lResultSet.getString(2), lResultSet.getString(3));
     	lRoomData.add(lSingleRoomData);
-          lResultSet.next();
+        lResultSet.next();
         }
+     	conn.close();
         } catch (Exception ex)
              {
                System.out.println("Error at the execution + " + "n" + ex.getMessage());
@@ -52,6 +55,44 @@ public class Room
      return lRoomData;
    }
 
+   public void create(int pId, String pName, String pAdditionalInfo) {
+	  try {
+	  conn = DBConnection.getConnection();
+	  PreparedStatement lStatement = conn.prepareStatement("INSERT INTO learningspaces.room VALUES ('"+Integer.toString(pId)+"','"+pName+"','"+pAdditionalInfo+"');");
+	  lStatement.executeUpdate();
+	  conn.close();
+	  } catch (Exception ex)
+      {
+        System.out.println("Error at the execution + " + "n" + ex.getMessage());
+      }
+   	}
+   
+   public void update(int pId, String pName, String pAdditionalInfo) {
+	  try {
+	  conn = DBConnection.getConnection();
+	  PreparedStatement lStatement = conn.prepareStatement("UPDATE learningspaces.room SET RoomNr = '"+pName+"', AdditionalInfo = '"+pAdditionalInfo+"' WHERE ID = '"+pId+"');");
+	  lStatement.executeUpdate();
+	  conn.close();
+	  } catch (Exception ex)
+      {
+        System.out.println("Error at the execution + " + "n" + ex.getMessage());
+      }
+   }
+   
+   public void delete(int pId) {
+	  try {
+	  conn = DBConnection.getConnection();
+	  PreparedStatement lStatement = conn.prepareStatement("DELETE FROM learningspaces.room WHERE ID = '"+pId+"');");
+	  lStatement.executeUpdate();
+	  conn.close();
+	  } catch (Exception ex)
+      {
+        System.out.println("Error at the execution + " + "n" + ex.getMessage());
+      }
+   }
+   
+   
+   
    // get/set
    public int getId()
    {
